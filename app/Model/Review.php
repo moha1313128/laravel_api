1d39f370132f661f46a592aa6a96fe99c81e7836
moha1313128 <moha1313128@gmail.com>
2019-10-20 11:10:12 +0100

    Create relationship between models

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace App\Model;
-
+use App\Model\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function reviews()
+    {
+    	return $this->belongsTo(Product::class);
+    }
 }
